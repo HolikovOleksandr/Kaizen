4611f75a36e9a746447dde5b9a9f69d7d249c9a6
Oleksandr <sanyagolikov97@gmail.com>
2023-10-29 10:52:04 +0200

    Add project files.

---
+++ Kaizen.MainWeb/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Kaizen.Core;
+using Kaizen.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaizen.MainWeb.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly KaizenDbContext _context;
+
+        public UserController(KaizenDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Created("", user);
+        }
+    }
+}
---
+++ Kaizen.MainWeb/Program.cs
@@ -0,0 +1,30 @@
+using Kaizen.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kaizen.MainWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+            builder.Services.AddDbContext<KaizenDbContext>(opt => opt.UseSqlServer(connectionString));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            Database.Migrate(app);
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
